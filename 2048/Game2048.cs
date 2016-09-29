3a2c8bb7ab5dddaff3a13e68b737d3ad12c3b11c
Lealcy Tardelli Belegante <lealcy@gmail.com>
2016-09-29 17:03:22 -0300

    Commit

---
+++ 2048/Game2048.cs
@@ -0,0 +1,163 @@
+﻿using System;
+
+namespace Application
+{
+    public class Game2048
+    {
+        private int width;
+        private int height;
+        private int[][] board;
+        public bool BoardChanged;
+        public int[][] Board { 
+            get { 
+                return board; 
+            }  
+            private set { 
+                board = value; 
+            } 
+        }
+
+        private Random rand;
+
+        public Game2048 (int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            board = new int[width][];
+            for (int x = 0; x < width; x++) {
+                board [x] = new int[height];
+                for (int y = 0; y < height; y++) {
+                    board [x] [y] = 0;
+                }
+            }
+            rand = new Random ();
+            AddValue ();
+            BoardChanged = true;
+        }
+
+        public void AddValue()
+        {
+            if (!IsBoardFull ()) {
+                int x, y;
+                do {
+                    x = rand.Next (width);
+                    y = rand.Next (height);
+                } while (board [x] [y] != 0);
+                board [x] [y] = rand.Next (4) > 2 ? 4 : 2;
+            }
+        }
+
+        public bool IsBoardFull() {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    if (board [x] [y] == 0) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void MoveUp() {
+            bool move;
+            do {
+                move = false;
+                for (int x = 0; x < width; x++) {
+                    for (int y = 1; y < height; y++) {
+                        if (board[x][y] > 0){
+                            if (board[x][y - 1] == 0) {
+                                swap (x, y, x, y - 1);
+                                move = true;
+                                BoardChanged = true;
+                            } else if (board[x][y] == board[x][y - 1]) {
+                                board[x][y] = 0;
+                                board[x][y - 1] *= 2;
+                                move = true;
+                                BoardChanged = true;
+                            }
+                        }
+                    }
+                }
+            } while (move == true);
+
+
+        }
+
+        public void MoveRight() {
+            bool move;
+            do {
+                move = false;
+                for (int y = 0; y < height; y++) {
+                    for (int x = width - 2; x >= 0; x--) {
+                        if (board[x][y] > 0){
+                            if (board[x + 1][y] == 0) {
+                                swap (x, y, x + 1, y);
+                                move = true;
+                                BoardChanged = true;
+                            } else if (board[x][y] == board[x + 1][y]) {
+                                board[x][y] = 0;
+                                board[x + 1][y] *= 2;
+                                move = true;
+                                BoardChanged = true;
+                            }
+                        }
+                    }
+                }
+            } while (move == true);
+        }
+
+        public void MoveDown() {
+            bool move;
+            do {
+                move = false;
+                for (int x = 0; x < width; x++) {
+                    for (int y = height - 2; y >= 0; y--) {
+                        if (board[x][y] > 0){
+                            if (board[x][y + 1] == 0) {
+                                swap (x, y, x, y + 1);
+                                move = true;
+                                BoardChanged = true;
+                            } else if (board[x][y] == board[x][y + 1]) {
+                                board[x][y] = 0;
+                                board[x][y + 1] *= 2;
+                                move = true;
+                                BoardChanged = true;
+                            }
+                        }
+                    }
+                }
+            } while (move == true);
+        }
+
+        public void MoveLeft() {
+            bool move;
+            do {
+                move = false;
+                for (int y = 0; y < height; y++) {
+                    for (int x = 1; x < height; x++) {
+                        if (board[x][y] > 0){
+                            if (board[x - 1][y] == 0) {
+                                swap (x, y, x - 1, y);
+                                move = true;
+                                BoardChanged = true;
+                            } else if (board[x][y] == board[x - 1][y]) {
+                                board[x][y] = 0;
+                                board[x - 1][y] *= 2;
+                                move = true;
+                                BoardChanged = true;
+                            }
+                        }
+                    }
+                }
+            } while (move == true);
+        }
+
+
+        private void swap(int x1, int y1, int x2, int y2) {
+            int temp = board[x1][y1];
+            board [x1] [y1] = board [x2] [y2];
+            board [x2] [y2] = temp;
+        }
+    }
+}
+
---
+++ 2048/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Application
+{
+    class MainClass
+    {
+        private static Dictionary<int, ConsoleColor> colors;
+
+        public static void Main (string[] args)
+        {
+            Console.CursorVisible = false;
+            colors = new Dictionary<int, ConsoleColor>();
+            colors [0] = ConsoleColor.White;
+            colors [2] = ConsoleColor.Gray;
+            colors [4] = ConsoleColor.Yellow;
+            colors [8] = ConsoleColor.DarkYellow;
+            colors [16] = ConsoleColor.Red;
+            colors [32] = ConsoleColor.DarkRed;
+            colors [64] = ConsoleColor.Magenta;
+            colors [128] = ConsoleColor.DarkMagenta;
+            colors [256] = ConsoleColor.Cyan;
+            colors [512] = ConsoleColor.DarkCyan;
+            colors [1024] = ConsoleColor.Blue;
+            colors [2048] = ConsoleColor.Green;
+
+            Game2048 g = new Game2048 (4, 4);
+            int x = Console.CursorLeft;
+            int y = Console.CursorTop;
+            drawBoard (g, x, y);
+            while (true) {
+                switch (Console.ReadKey (true).Key) {
+                case ConsoleKey.Escape:
+                case ConsoleKey.Enter:
+                    return;
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.W:
+                    g.MoveUp ();
+                    break;
+                case ConsoleKey.RightArrow:
+                case ConsoleKey.D:
+                    g.MoveRight ();
+                    break;
+                case ConsoleKey.DownArrow:
+                case ConsoleKey.S:
+                    g.MoveDown ();
+                    break;
+                case ConsoleKey.LeftArrow:
+                case ConsoleKey.A:
+                    g.MoveLeft ();
+                    break;
+                }
+                if (g.IsBoardFull ()) {
+                    drawBoard (g, x, y);
+                    Console.WriteLine ("\nGAME OVER!");
+                    return;
+                }
+                if (g.BoardChanged) {
+                    g.AddValue ();
+                    g.BoardChanged = false;
+                }
+                drawBoard (g, x, y);
+                
+            }
+
+
+        }
+
+        private static void drawBoard(Game2048 g, int x, int y) {
+            for (int i = 0; i < g.Board.Length; i++) {
+                for (int j = 0; j < g.Board[i].Length; j++) {
+                    if (colors.ContainsKey (g.Board [i] [j])) {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = colors [g.Board [i] [j]];
+                    } else {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                    Console.SetCursorPosition (x + i * 6, y + j * 3);
+                    Console.Write ("      ");
+                    Console.SetCursorPosition (x + i * 6, (y + j * 3) + 1);
+                    Console.Write ("{0, 5} ", g.Board[i][j]);
+                    Console.SetCursorPosition (x + i * 6, (y + j * 3) + 2);
+                    Console.Write ("      ");
+                    Console.ResetColor ();
+                }
+            }
+        }
+    }
+}
